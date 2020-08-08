ed18b0b6354d63457d71302a6597c25abf9aafff
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2020-08-08 15:01:19 +0200

    adjusted script to work

--- nodered-health.py
+++ nodered-health.py
@@ -1,12 +1,15 @@
+import subprocess
 import paho.mqtt.client as mqtt
 
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code " + str(rc))
+client = mqtt.Client()
 
-def publish_health():
-  client = mqtt.Client()
-  client.on_connect = on_connect
+def publish_cpu_temperature():
+  p = subprocess.Popen('vcgencmd measure_temp', shell=True, stdout=subprocess.PIPE)
+  client.publish('/home/pis/nodered/cpu/temperature', payload=p.stdout.read()[5:-3], qos=1)
+
+def initialize():
   client.connect('127.0.0.1', 1883)
+  publish_cpu_temperature()
 
 if __name__ == '__main__':
-  publish_health()
+  initialize()
