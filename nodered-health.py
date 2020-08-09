f156d684bfef45d8a2bb42c9f5ea7976b7a37d5b
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2020-08-09 12:14:27 +0200

    gather cpu stats

--- nodered-health.py
+++ nodered-health.py
@@ -13,6 +13,10 @@ def gather_and_publish():
   p = subprocess.Popen('vcgencmd measure_temp', shell=True, stdout=subprocess.PIPE)
   data['cpu']['temperature'] = p.stdout.read()[5:-3].decode('utf-8')
 
+  # cpu stats
+  p = subprocess.Popen('mpstat --dec=2 -P 0 -o JSON', shell=True, stdout=subprocess.PIPE)
+  data['cpu']['stats'] = p.stdout.read().decode('utf-8')
+
   # publish as a single data set
   client.publish('/home/pis/nodered/health', payload=json.dumps(data), qos=1)
 
