ace3c0cd768617fa8947d6d995da568cbd524b77
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2020-09-01 19:42:05 +0200

    reordered commands

--- setup/setup-docker.sh
+++ setup/setup-docker.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-echo "> Installing prerequisites..."
-sudo apt update
-# necessary packages:
-# docker: apt-transport-https ca-certificates software-properties-common
-# docker-compose: libffi-dev libssl-dev python3-dev python3 python3-pip
-sudo apt install -y && \
-  apt-transport-https ca-certificates software-properties-common && \
-  libffi-dev libssl-dev python3-dev python3 python3-pip
-echo "============================================================"
-
 # ensure certificates are alright - it is always wrong in the image
 echo "> Ensuring correct ca-certificates..."
 sudo apt install --reinstall ca-certificates && sudo c_rehash
 echo "============================================================"
 
+echo "> Installing prerequisites..."
+sudo apt update
+# necessary packages:
+# docker: software-properties-common
+# docker-compose: libffi-dev libssl-dev python3-dev python3 python3-pip
+sudo apt install -y && \
+  software-properties-common && \
+  libffi-dev libssl-dev python3-dev python3 python3-pip
+echo "============================================================"
 
 echo "> Installing Docker..."
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
